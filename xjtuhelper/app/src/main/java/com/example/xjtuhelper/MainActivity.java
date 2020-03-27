a6a2c9ae65a3daf9eb78f7ea4cf376f6fe0d742d
nightmorning0 <nightmorning@qq.com>
2020-03-27 18:09:10 +0800

    Basic listview function completed.

---
+++ xjtuhelper/app/src/main/java/com/example/xjtuhelper/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.xjtuhelper;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.List;
+import android.widget.Toast;
+import android.view.View;
+import android.widget.AdapterView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private List<String> newstitle;
+    private ListView newslist;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        newstitle = new ArrayList<String>();
+        newstitle.add("关于启用雨课堂“长江”分平台的通知");
+        newstitle.add("浏览器跳转测试");
+        for (int i = 3; i < 10; i++){
+            newstitle.add("第" + i + "条新闻");
+        }
+        // 设置 adapter
+        ListAdapter adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, newstitle);
+        newslist = (ListView) findViewById(R.id.news_list);
+        newslist.setAdapter(adapter);
+        newslist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (id == 0) {
+                    ;
+                }
+                else if (id == 1){
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://nightmorning.site")));
+                }
+            }
+        });
+    }
+}
